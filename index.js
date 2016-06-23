672c69fd2a8890bbe192a62523c7e106e26d6c54
yinyong02 <yinyong02@baidu.com>
2016-06-23 19:54:13 +0800

    fixed isSkip;lock options

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 /**
-  * Copyright (C) 2016 pentojs.xyz
-  * transformer.js
-  *
-  * changelog
-  * 2016-06-21[19:30:48]:revised
-  *
-  * @author yanni4night@gmail.com
-  * @version 1.0.0
-  * @since 1.0.0
-  */
+ * Copyright (C) 2016 pantojs.xyz
+ * transformer.js
+ *
+ * changelog
+ * 2016-06-21[19:30:48]:revised
+ *
+ * @author yanni4night@gmail.com
+ * @version 0.1.2
+ * @since 0.1.0
+ */
 'use strict';
 
 class Transformer {
@@ -16,11 +16,17 @@ class Transformer {
         if (!panto.util.isNil(opt) && !panto.util.isPlainObject(opt)) {
             throw new Error(`A PLAIN OBJECT is required to construct a transformer`);
         }
-        this.options = panto.util.extend({}, opt);
+
+        Object.defineProperty(this, 'options', {
+            value: panto.util.extend({}, opt),
+            writable: false,
+            configurable: false,
+            enumerable: true
+        });
     }
     transform(file) {
         if (panto.util.isNil(file) || true === this.options.isSkip) {
-            return Promise.resolve([]);
+            return Promise.resolve(file);
         }
 
         return this._transform(file);
